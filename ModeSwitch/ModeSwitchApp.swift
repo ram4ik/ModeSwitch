795722c44110f123cda42a0bc5cfb687988550a7
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-23 22:30:50 +0300

    Initial Commit

---
+++ ModeSwitch/ModeSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModeSwitchApp.swift
+//  ModeSwitch
+//
+//  Created by Ramill Ibragimov on 23.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct ModeSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
